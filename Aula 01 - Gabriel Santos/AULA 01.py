fa81ad98b566441bcfbd7ad1570dd1032024468d
Gaabee-hash <gabriielsantoss088@gmail.com>
2024-11-25 15:49:14 -0300

    Add files via upload
    Aula 1 python

---
+++ Aula 01 - Gabriel Santos/AULA 01.py
@@ -0,0 +1,5 @@
+print('Olá, mundo')
+print('Foda-se')
+print('Um dia um gago me falou que a mulher que ele tanto amou fofofoi fofofofofofoi fofofofoi embora.')
+print('I am Batman')
+print('Eu tenho aids')
